1f5cc6e2a07628199a383b8bebe19892a95443ac
Colbe Roberson <cgr28@njit.edu>
2022-11-04 00:57:07 -0400

    added "catch-all" for routing

--- backend/app.py
+++ backend/app.py
@@ -9,5 +9,9 @@ CORS(app, origin="*")
 def serve():
     return send_from_directory(app.static_folder, "index.html")
 
+@app.route("/*")
+def serve():
+    return send_from_directory(app.static_folder, "index.html")
+
 if __name__=="__main__":
     app.run(host="localhost", port="8080", debug=True)
