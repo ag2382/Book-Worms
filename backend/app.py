efa0d1330611515e045bd7d88110bfdcc4da3e94
Colbe Roberson <cgr28@njit.edu>
2022-11-10 00:51:07 -0500

    updated call all

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,24 @@
 from flask import Flask, send_from_directory, request
 from flask_cors import CORS
+import os
 
 app = Flask(__name__, static_folder="../build", static_url_path="/")
 
 CORS(app, origin="*")
 
-@app.route("/")
-def serve():
-    return send_from_directory(app.static_folder, "index.html")
-
-@app.route("/profile")
-def profile():
-    return send_from_directory(app.static_folder, "index.html")
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    print("Catch all.", path)
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(os.path.join(app.static_folder), path)
+    else:
+        return send_from_directory(os.path.join(app.static_folder),'index.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
     """When there's a 404 error, return to the main page."""
+    print("Page not found.", e)
     return send_from_directory(app.static_folder, "index.html")
 
 if __name__=="__main__":
