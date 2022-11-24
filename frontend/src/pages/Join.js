b5500a67f5f590895212ab948a341f2dfa791aad
Colbe Roberson <cgr28@njit.edu>
2022-11-23 22:32:45 -0500

    added page for joining clubs

---
+++ frontend/src/pages/Join.js
@@ -0,0 +1,10 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+
+export default function Create() {
+    return (
+        <div className="mt-3">
+            Join a club
+        </div>
+    )
+}
