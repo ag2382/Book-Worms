35e1f29fffad6160518dff54d2cc00bd8a8d144c
Mostafa Damir <mad237@njit.edu>
2022-11-30 23:00:54 -0500

    added a .env file making everything more secure

--- backend/best_sellers.py
+++ backend/best_sellers.py
@@ -1,8 +1,8 @@
 import requests 
-from secrets import BOOK_API_KEY
+from decouple import config
 url = "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json"
-
-concat = f"{url}?age-group=&author=&contributor=&isbn=&offset=&price=&publisher=&title=&api-key={BOOK_API_KEY}"
+key = config('BOOK_API_KEY')
+concat = f"{url}?age-group=&author=&contributor=&isbn=&offset=&price=&publisher=&title=&api-key={key}"
 r=requests.get(concat)
 print('Response Code:', r.status_code)
 print('Response Headers:\n', r.headers)
--- backend/lists.py
+++ backend/lists.py
@@ -1,8 +1,8 @@
 import requests 
-from secrets import BOOK_API_KEY
+from decouple import config
+key = config('BOOK_API_KEY')
 url = "https://api.nytimes.com/svc/books/v3/lists.json"
-
-concat = f"{url}?list-name=hardcover-fiction&api-key={BOOK_API_KEY}"
+concat = f"{url}?list-name=hardcover-fiction&api-key={key}"
 r=requests.get(concat)
 print('Response Code:', r.status_code)
 print('Response Headers:\n', r.headers)
